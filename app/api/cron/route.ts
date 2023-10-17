2622be840636099ed0b504e95956f9312d5f3532
Victor <matheus0588@outlook.com>
2023-10-17 18:18:49 -0300

    fix errors

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -11,6 +11,9 @@ import {
   getLowestPrice,
 } from "@/lib/utils";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export async function GET(request: Request) {
   try {
     connectToDB();
